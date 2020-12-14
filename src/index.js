b364aa459455c8edc67b6685f082b1f7e4ca5a3a
leitegs <leitedasilvag@gmail.com>
2020-12-14 11:30:01 -0300

    started server

--- src/index.js
+++ src/index.js
@@ -1,53 +1,6 @@
-import { ApolloServer, gql } from 'apollo-server';
+import startServer from './apollo/startServer';
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: Author
-  }
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
-  type Author {
-    name: String
-    books: [Book]
-  }
-
-  type Query {
-    books: [Book]
-    authors: [Author]
-  }
-`
-
-const authors = [
-  {
-    name: 'Kate Chopin',
-    books: ['The Awakening']
-  },
-  {
-    name: 'Paul Auster',
-    books: ['City of Glass']
-  }
-];
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: authors[0],
-  },
-  {
-    title: 'City of Glass',
-    author: authors[1],
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-    authors: () => authors
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers })
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+startServer({ typeDefs, resolvers }); 
