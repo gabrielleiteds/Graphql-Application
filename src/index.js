6e18ad9b1802685e37f92008566ea94969825077
leitegs <leitedasilvag@gmail.com>
2020-12-14 20:22:40 -0300

    create connection with database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 import startServer from './apollo/startServer';
 
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
+
+import('./database/connection')
 
 startServer({ typeDefs, resolvers }); 
