da5baa25cda0b8edd2134e0b7ec27d4b3453ba29
leitegs <leitedasilvag@gmail.com>
2020-12-14 19:34:33 -0300

    configure resolvers and merge resolvers of posts and users

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,1 +1,8 @@
-export default {}; 
+import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
+
+// import of automatic form files with .gql extension
+const resolversArray = fileLoader(path.join(__dirname, 'modules', '**', 'resolvers.js'))
+const resolvers = mergeResolvers(resolversArray)
+
+export default resolvers; 
