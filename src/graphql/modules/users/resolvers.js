c28fabf1b0d7834522fd38ba9d4029866bbb0cc2
leitegs <leitedasilvag@gmail.com>
2020-12-16 09:48:03 -0300

    create subscription for Supervising in real-time creation of users utilities webSockets

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,17 +1,30 @@
 import User from '../../../models/User';
-
+import { USER_ADDED } from './channels';
 export default {
   Query: {
     users: () => User.find(),
     user: (_, { id }) => User.findById(id)
   },
   Mutation: {
-    createUser: async (_, { data }) => await User.create(data),
+    createUser: async (_, { data }, { pubSub }) => {
+      const user = await User.create(data)
+
+      pubSub.publish(USER_ADDED, {
+        userAdded: user,
+      })
+
+      return user;
+    },
     updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => {
       const deleted = await User.findOneAndDelete(id)
 
       return !!deleted;
+    }
+  },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj, args, { pubSub }) => pubSub.asyncIterator(USER_ADDED)
     }
   }
 }; 
